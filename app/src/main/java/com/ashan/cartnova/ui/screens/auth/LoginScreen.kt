23bb443d69c44d5baa10c446d2756437ee01408b
ashanbashi <ashanbashi23@gmail.com>
2026-04-10 17:44:46 +0300

    first commit

--- app/src/main/java/com/ashan/cartnova/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/ashan/cartnova/ui/screens/auth/LoginScreen.kt
@@ -1,16 +1,121 @@
 package com.ashan.cartnova.ui.screens.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ashan.cartnova.R
+import com.ashan.cartnova.ui.theme.Pink20
+import com.ashan.cartnova.ui.theme.Pink40
 
 @Composable
 fun LoginScreen(){
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(painter = painterResource(R.drawable.bg), contentScale = ContentScale.FillBounds),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
+
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = "product",
+            modifier = Modifier.size(300.dp).clip(shape = RoundedCornerShape(20.dp))
+
+        )
+
+        Text(
+            text = "Login To Start Your Journey Today",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        //Variables
+
+
+        var email by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it},
+            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+            placeholder = { Text(text = "example@gmail.com") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Pink40,
+                focusedBorderColor = Pink20,
+                focusedLeadingIconColor = Pink20
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var password by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it},
+            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+            label = { Text(text = "Password") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Pink40,
+                focusedBorderColor = Pink20,
+                focusedLeadingIconColor = Pink20
+            ),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(Pink40),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.width(150.dp),
+        ) {
+            Text(text = "Log In")
+        }
 
     }
 }
--- app/src/main/java/com/ashan/cartnova/ui/screens/auth/RegisterScreen.kt
+++ app/src/main/java/com/ashan/cartnova/ui/screens/auth/RegisterScreen.kt
@@ -1,5 +1,6 @@
 package com.ashan.cartnova.ui.screens.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,12 +8,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -22,18 +31,37 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ashan.cartnova.R
+import com.ashan.cartnova.ui.theme.Pink20
+import com.ashan.cartnova.ui.theme.Pink40
 
 @Composable
 fun RegisterScreen(){
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(painter = painterResource(R.drawable.bg), contentScale = ContentScale.FillBounds),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = "product",
+            modifier = Modifier.size(300.dp).clip(shape = RoundedCornerShape(20.dp))
+
+        )
 
         Text(
             text = "Join Us And Start Your Journey Today",
@@ -46,42 +74,87 @@ fun RegisterScreen(){
         //Variables
 
         var username by remember { mutableStateOf("") }
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var confirmpassword by remember { mutableStateOf("") }
 
-        TextField(
+        OutlinedTextField(
             value = username,
             onValueChange = { username = it},
             modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
             label = { Text(text = "Username") },
             leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "") },
-
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Pink40,
+                focusedBorderColor = Pink20,
+                focusedLeadingIconColor = Pink20
+            )
         )
 
-        TextField(
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var email by remember { mutableStateOf("") }
+
+        OutlinedTextField(
             value = email,
             onValueChange = { email = it},
             modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
             placeholder = { Text(text = "example@gmail.com") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") }
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Pink40,
+                focusedBorderColor = Pink20,
+                focusedLeadingIconColor = Pink20
+            )
         )
 
-        TextField(
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var password by remember { mutableStateOf("") }
+
+        OutlinedTextField(
             value = password,
             onValueChange = { password = it},
             modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
             label = { Text(text = "Password") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") }
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Pink40,
+                focusedBorderColor = Pink20,
+                focusedLeadingIconColor = Pink20
+            ),
+            visualTransformation = PasswordVisualTransformation()
         )
 
-        TextField(
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var confirmpassword by remember { mutableStateOf("") }
+
+        OutlinedTextField(
             value = confirmpassword,
             onValueChange = { confirmpassword = it},
             modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
             label = { Text(text = "Confirm Password") },
-            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") }
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Pink40,
+                focusedBorderColor = Pink20,
+                focusedLeadingIconColor = Pink20
+            ),
+            visualTransformation = PasswordVisualTransformation()
         )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(Pink40),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.width(150.dp),
+        ) {
+            Text(text = "Sign Up")
+        }
 
     }
 }
